f3f93b0d61046fc94c8bc56ad7431259694e9673
giacomo <pellicci.giac@gmail.com>
2019-12-04 18:38:42 +0100

    intermediate version for tabellino

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 import json
+import os
 import time
+import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -15,9 +17,14 @@ def wait_for_ajax(driver):
     except Exception as e:
         pass
 
+months = ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"]
+months_num = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 
 f = open("test.txt", "w+")
-browser = webdriver.Chrome()
+webdriverPath = "./chromedriver"
+if os.name == 'nt':
+    webdriverPath += ".exe"
+browser = webdriver.Chrome(webdriverPath)
 browser.maximize_window()
 url = "https://www.sofascore.com/it/torneo/calcio/italy/serie-a/23"
 start_time = time.time()
@@ -32,7 +39,7 @@ ul = browser.find_element_by_xpath(
 li = ul.find_elements_by_tag_name("li")
 dropdown.click()
 li[3].find_element_by_tag_name("a").click()
-print "Clicked on season " + dropdown.text
+print("Clicked on season " + dropdown.text)
 
 # set week 1
 wait_for_ajax(browser)
@@ -47,7 +54,7 @@ li[0].find_element_by_tag_name("a").click()
 # loop through the matches to scrape data
 wait_for_ajax(browser)
 matches = browser.find_elements_by_class_name("js-event-list-tournament-events")[1].find_elements_by_tag_name("a")
-print len(matches)
+print(len(matches))
 browser.find_element_by_xpath(
     "//*[@id='pjax-container-main']/div/div[2]/div/div/div/div[1]/div[3]/div[3]/div/div[2]/div/div/button").click()
 for i in range(0, len(matches)):
@@ -69,7 +76,16 @@ for i in range(0, len(matches)):
     # get match date
     date = browser.find_element_by_class_name("js-details-component-startTime-container").find_elements_by_class_name(
         "cell__content")[0]
-    data['date'] = date.text
+    d = str(date.text).replace(".", "").replace(",", "")
+    d = d.split()
+    isodate = d[2] + "-" + months_num[months.index(d[1])] + "-" + d[0] + "T" + d[3] + ":00:000+01:00"
+    data['date'] = isodate
+    #get match events (tabellino)
+    #div = browser.find_element_by_class_name("incidents-container")
+    ##############
+    #events = div.find_elements_by_class_name("cell")
+    #print("Events#"+len(events))
+    ############### prendere cell (center, right, left) e scriverci away o home
     # click statistics
     browser.find_element_by_xpath("//*[@id='pjax-container-main']/div/div[2]/div/div/div/div[1]/div[3]/div[3]/div/div[2]/div/div/div[1]/div[3]/ul/li[2]").click()
     wait_for_ajax(browser)
@@ -86,7 +102,8 @@ for i in range(0, len(matches)):
                 data['away' + statName] = e
 
     json_data = json.dumps(data)
-    print json_data
+    print(json_data)
+    #click X and close stats panel
     browser.find_element_by_xpath(
         "//*[@id='pjax-container-main']/div/div[2]/div/div/div/div[1]/div[3]/div[3]/div/div[2]/div/div/button").click()
 
