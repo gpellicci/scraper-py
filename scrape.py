14175dde7f54c94cff398d53d9b0ae26b9f5f935
Polarstrike <Polarstrike>
2019-12-05 01:10:18 +0100

    scrape match info

--- scrape.py
+++ scrape.py
@@ -48,83 +48,94 @@ dropdown = dropdown[1]
 ul = dropdown.find_element_by_tag_name("ul")
 li = ul.find_elements_by_tag_name("li")
 browser.execute_script("arguments[0].scrollIntoView(true);", dropdown)
-dropdown.click()
-li[0].find_element_by_tag_name("a").click()
-
-#for i in range(0, len(li)):
-    #li[i].find_element_by_tag_name("a").click()
-# loop through the matches to scrape data
-wait_for_ajax(browser)
-matches = browser.find_elements_by_class_name("js-event-list-tournament-events")[1].find_elements_by_tag_name("a")
-print(len(matches))
-browser.find_element_by_xpath(
-    "//*[@id='pjax-container-main']/div/div[2]/div/div/div/div[1]/div[3]/div[3]/div/div[2]/div/div/button").click()
-for i in range(0, len(matches)):
-    matches_click = browser.find_elements_by_class_name("js-event-list-tournament-events")[1].find_elements_by_tag_name(
-        "a")
-    matches_click[i].click()
-    try:
-        element = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "js-details-widget-container"))
-        )
-    except:
-        pass
+browser.find_element_by_class_name("widget-close-button").click()
+print("Composed of " + len(li) + " weeks")
+for j in range(0, len(li)):
+    dropdown.click()
+    li[j].find_element_by_tag_name("a").click()
+    # loop through the matches to scrape data
     wait_for_ajax(browser)
-    # get team names
-    teams = str(browser.find_element_by_css_selector("a.h-interactive.js-event-link").text).split(" - ")
-    data = {}
-    data['homeTeam'] = teams[0]
-    data['awayTeam'] = teams[1]
-    # get match date
-    date = browser.find_element_by_class_name("js-details-component-startTime-container").find_elements_by_class_name(
-        "cell__content")[0]
-    d = str(date.text).replace(".", "").replace(",", "")
-    d = d.split()
-    isodate = d[2] + "-" + months_num[months.index(d[1])] + "-" + d[0] + "T" + d[3] + ":00:000+01:00"
-    data['date'] = isodate
-    #get match events (tabellino)
-    div = browser.find_element_by_class_name("incidents-container")
-    events = div.find_elements_by_class_name("cell--incident")
-    for i, event in enumerate(events):
-        browser.execute_script("arguments[0].scrollIntoView(true);", events[i])
-        divClass = event.get_attribute("class")
-        txt = str(event.text).replace('\n', ' ').replace(" +", "+")
-        if len(txt) == 0:
+    matches = browser.find_elements_by_class_name("js-event-list-tournament-events")
+    for m in matches:
+        if (m.is_displayed()):
+            matches_click = m
+            break
+    matches_click = matches_click.find_elements_by_tag_name("a")
+    #browser.find_element_by_xpath("//*[@id='pjax-container-main']/div/div[2]/div/div/div/div[1]/div[3]/div[3]/div/div[2]/div/div/button").click()
+    for i in range(0, len(matches_click)):
+        #j+1 because the website adds a new div for each click on the week dropdown and never delete the old one
+        matches_click[i].click()
+        try:
+            element = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "js-details-widget-container"))
+            )
+        except:
             pass
-        elif "cell--right" in divClass:
-            data['eventAway'+str(i)] = txt
-        elif "cell--center" in divClass:
-            data['event'+str(i)] = txt
-        else:
-            data['eventHome'+str(i)] = txt
-    #scroll back to top of window
-    browser.execute_script("arguments[0].scrollIntoView(true);", browser.find_element_by_css_selector("a.h-interactive.js-event-link"))
-    # click statistics
-    browser.find_element_by_xpath("//*[@id='pjax-container-main']/div/div[2]/div/div/div/div[1]/div[3]/div[3]/div/div[2]/div/div/div[1]/div[3]/ul/li[2]").click()
-    wait_for_ajax(browser)
-    # get stats
-    stats = browser.find_element_by_id("statistics-period-ALL").find_elements_by_class_name("stat-group-event")
-    for s in stats:
-        for i, e in enumerate(str(s.text).split('\n')):
-            if i % 3 == 0:
-                tmp = e
-            elif i % 3 == 1:
-                statName = e.title().replace(' ', '').replace("'", "")  # CamelCase it
-                data['home' + statName] = tmp
+        wait_for_ajax(browser)
+        # get team names
+        teams = str(browser.find_element_by_css_selector("a.h-interactive.js-event-link").text).split(" - ")
+        data = {}
+        data['homeTeam'] = teams[0]
+        data['awayTeam'] = teams[1]
+        # get match date
+        date = browser.find_element_by_class_name("js-details-component-startTime-container").find_elements_by_class_name(
+            "cell__content")[0]
+        d = str(date.text).replace(".", "").replace(",", "")
+        d = d.split()
+        isodate = d[2] + "-" + months_num[months.index(d[1])] + "-" + d[0] + "T" + d[3] + ":00:000+01:00"
+        data['date'] = isodate
+        #get match events (tabellino)
+        div = browser.find_element_by_class_name("incidents-container")
+        events = div.find_elements_by_class_name("cell--incident")
+        for i, event in enumerate(events):
+            browser.execute_script("arguments[0].scrollIntoView(true);", events[i])
+            divClass = event.get_attribute("class")
+            txt = str(event.text).replace('\n', ' ').replace(" +", "+")
+            if len(txt) == 0:
+                pass
+            elif "cell--right" in divClass:
+                data['eventAway'+str(i)] = txt
+            elif "cell--center" in divClass:
+                data['event'+str(i)] = txt
             else:
-                data['away' + statName] = e
+                data['eventHome'+str(i)] = txt
+        #get match info like stadium and location
+        matchInfo = browser.find_element_by_class_name("js-event-page-info-container")
+        browser.execute_script("arguments[0].scrollIntoView(true);", matchInfo)
+        matchInfo = matchInfo.find_elements_by_tag_name("tr")
+        for m in matchInfo:
+            td = m.find_elements_by_tag_name("td")
+            td[0] = str(td[0].text).replace("\n", " ").title().replace(' ', '').replace("'", "")
+            td[1] = str(td[1].text).replace("\n", " ")
+            if "DataDiInizio" != td[0] and "CartelliniPerPartita" != td[0]:
+                data['info' + td[0]] = td[1]
 
-    json_data = json.dumps(data)
-    print(json_data)
-    f.write(json_data)
-    #click X and close stats panel
-    browser.find_element_by_xpath(
-        "//*[@id='pjax-container-main']/div/div[2]/div/div/div/div[1]/div[3]/div[3]/div/div[2]/div/div/button").click()
 
-ul = dropdown.find_element_by_tag_name("ul")
-li = ul.find_elements_by_tag_name("li")
-dropdown.click()
+        #scroll back to top of window
+        browser.execute_script("arguments[0].scrollIntoView(true);", browser.find_element_by_css_selector("a.h-interactive.js-event-link"))
+        # click statistics
+        browser.find_element_by_xpath("//*[@id='pjax-container-main']/div/div[2]/div/div/div/div[1]/div[3]/div[3]/div/div[2]/div/div/div[1]/div[3]/ul/li[2]").click()
+        wait_for_ajax(browser)
+        # get stats
+        stats = browser.find_element_by_id("statistics-period-ALL").find_elements_by_class_name("stat-group-event")
+        for s in stats:
+            browser.execute_script("arguments[0].scrollIntoView(true);", s)
+            for i, e in enumerate(str(s.text).split('\n')):
+                if i % 3 == 0:
+                    tmp = e
+                elif i % 3 == 1:
+                    statName = e.title().replace(' ', '').replace("'", "")  # CamelCase it
+                    data['home' + statName] = tmp
+                else:
+                    data['away' + statName] = e
 
+        #scroll back to top of window
+        browser.execute_script("arguments[0].scrollIntoView(true);", browser.find_element_by_css_selector("a.h-interactive.js-event-link"))
+        json_data = json.dumps(data)
+        print(json_data)
+        f.write(json_data + '\n')
+        #click X and close stats panel
+        browser.find_element_by_class_name("widget-close-button").click()
 
 
 f.close()
